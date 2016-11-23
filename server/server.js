fc3c80ecb6d85255e4d768d77eaafefbbf6181df
Yudin Nikita <nekitz.1511@gmail.com>
2016-11-23 15:55:52 +0300

    do not work

---
+++ server/server.js
@@ -0,0 +1,21 @@
+var fs = require('fs');
+var https = require('https');
+var options = {
+    key: fs.readFileSync('certs/serverKey.key'),
+    cert: fs.readFileSync('certs/server.crt'),
+    ca: fs.readFileSync('certs/CA.crt')
+};
+
+var server = https.createServer(options, function (req, res) {
+    var body = [];
+    req.on('error', function(err) {
+        console.error(err);
+    }).on('data', function(chunk) {
+        body.push(chunk);
+    }).on('end', function() {
+        body = Buffer.concat(body).toString();
+        console.log(body + new Date());
+    });
+}).listen(8000);
+
+
